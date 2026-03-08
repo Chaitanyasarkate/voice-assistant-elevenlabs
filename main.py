97c4155dbf1e038124b2e3385d513495e1842482
Chaitanya Sarkate <155342826+Chaitanyasarkate@users.noreply.github.com>
2026-03-08 16:25:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import streamlit as st
 import google.generativeai as genai
 from elevenlabs.client import ElevenLabs
-from elevenlabs import play
 
 ELEVENLABS_API_KEY = st.secrets["ELEVENLABS_API_KEY"]
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 
 genai.configure(api_key=GOOGLE_API_KEY)
+
 model = genai.GenerativeModel("gemini-pro")
 
 eleven = ElevenLabs(api_key=ELEVENLABS_API_KEY)
 
-st.title("Jarvis AI Voice Assistant")
+st.title("AI Voice Assistant")
 
 user_input = st.text_input("Ask something")
 
 if st.button("Ask"):
 
-    response = model.generate_content(user_input)
+    if not user_input.strip():
+        st.warning("Please enter a question")
+    
+    else:
+        try:
+            response = model.generate_content(user_input)
 
-    answer = response.text
+            answer = response.text
 
-    st.write("Assistant:", answer)
+            st.write("Assistant:", answer)
 
-    audio = eleven.text_to_speech.convert(
-        voice_id="21m00Tcm4TlvDq8ikWAM",
-        text=answer
-    )
-
-    play(audio)
+        except Exception as e:
+            st.error("AI service error. Please try again.")
+            st.text(e)
